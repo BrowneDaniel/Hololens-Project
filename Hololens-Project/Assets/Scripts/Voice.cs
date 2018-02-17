ed85e1c9c6ddeaf3cd98fac26d90a5739b4e49c4
Brian Whelan <whelanb7@tcd.ie>
2018-02-17 14:16:12 +0000

    Integrated call to MS Translator API in Voice.cs
    Once dictation is complete, a second UIText will appear with the
    translated text based on translatingLanguage property

--- Hololens-Project/Assets/Scripts/Voice.cs
+++ Hololens-Project/Assets/Scripts/Voice.cs
@@ -1,13 +1,39 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Networking;
 using UnityEngine.Windows.Speech;
 using HoloToolkit.Unity.InputModule;
+using System.Collections;
 
 public class Voice : MonoBehaviour
 {
     private DictationRecognizer m_DictationRecognizer;
     public GameObject t;
+    public GameObject translatedT;
     public SpeechInputSource keywordRecogiser;
+    public string apiKey;
+    public string dictatingLanguage;
+    public string translatingLanguage;
+
+    public IEnumerator Translate(string text)
+    {
+        string urlEncodedText = WWW.EscapeURL(text);
+        string url = "https://api.microsofttranslator.com/V2/Http.svc/Translate?to=" + translatingLanguage + "&from=" + dictatingLanguage + "&text=" + urlEncodedText;
+        //string url = "http://brianw.netsoc.ie/telecomms.html";
+        UnityWebRequest translate = UnityWebRequest.Get(url);
+        translate.SetRequestHeader("Ocp-Apim-Subscription-Key", apiKey);
+        yield return translate.SendWebRequest();
+        if (translate.isNetworkError || translate.isHttpError)
+        {
+            Debug.Log(translate.error);
+        }
+        else
+        {
+            string parsedResponse = translate.downloadHandler.text.Split('>')[1].Split('<')[0];
+            Debug.Log(parsedResponse);
+            translatedT.GetComponent<Text>().text = parsedResponse;
+        }
+    }
     public void Dictate()
     {
         Debug.Log("Started Dictation");
@@ -18,6 +44,7 @@ public class Voice : MonoBehaviour
             {
                 Debug.LogFormat("Dictation result: {0}", text);
                 t.GetComponent<Text>().text = text;
+                StartCoroutine(Translate(text));
             };
 
         m_DictationRecognizer.DictationHypothesis += (text) =>
